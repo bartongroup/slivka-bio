d6e5d30a8841ebaf5e30b06b0618c70b8aa985a6
Mateusz Warowny <warownia1@gmail.com>
2018-08-21 14:25:27 +0100

    Update for 0.3a2 compatibility

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,4 @@
-import logging.config
 import os
-
-import slivka
 
 SETTINGS_FILE = 'settings.yml'
 
@@ -9,19 +6,9 @@ settings_path = os.path.join(
     os.path.dirname(os.path.abspath(__file__)),
     SETTINGS_FILE
 )
-slivka.settings.read_yaml_file(settings_path)
-slivka.settings.init_logs()
-logging.config.dictConfig(slivka.settings.LOGGER_CONFIG)
+os.environ.setdefault('SLIVKA_SETTINGS', settings_path)
 
-from slivka.server.forms import FormFactory
+
 from slivka.server.serverapp import app
 
-form_factory = FormFactory()
-for configuration in slivka.settings.service_configurations.values():
-    form_factory.add_form(configuration)
-
-app.config.update(
-    DEBUG=False,
-    MEDIA_DIR=slivka.settings.MEDIA_DIR,
-    SECRET_KEY=slivka.settings.SECRET_KEY
-)
+application = app
