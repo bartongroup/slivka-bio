900ddf460687ca6754c7eb8353a48994c6a735d4
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2019-10-17 15:16:37 +0100

    JPred command update

--- scripts/runners.py
+++ scripts/runners.py
@@ -1,6 +1,7 @@
 import os
 import re
 import requests
+import tarfile
 
 from slivka.scheduler import Runner
 from slivka.utils import JobStatus
@@ -9,11 +10,12 @@ from slivka.utils import JobStatus
 class JPredRunner(Runner):
     _jpred4="http://www.compbio.dundee.ac.uk/jpred4"
     _host = "http://www.compbio.dundee.ac.uk/jpred4/cgi-bin/rest"
+    FAILED_ID = 'jp_fail'
 
     def submit(self, cmd, cwd):
-        with open(cmd[1]) as fp:
+        with open(cmd[-1]) as fp:
             seq = fp.read()
-        content = str.join("£€£€", ['skipPDB=on', 'format=msf', seq])
+        content = str.join("£€£€", [*cmd[1:-1], seq])
         response = requests.post(
             '%s/job' % self._host,
             data=content.encode(),
@@ -21,11 +23,21 @@ class JPredRunner(Runner):
         )
         response.raise_for_status()
         result_url = response.headers['Location']
-        job_id = re.search(r'jp_.*$', result_url).group()
-        return job_id
+        with open(os.path.join(cwd, 'stdout'), 'wb') as fp:
+            fp.write(response.content)
+        try:
+            job_id = re.search(r'jp_.*$', result_url).group()
+            return job_id
+        except AttributeError:
+            return self.FAILED_ID
 
     @classmethod
     def check_status(cls, job_id, cwd) -> JobStatus:
+        if job_id == cls.FAILED_ID:
+            return JobStatus.FAILED
+        tarball = os.path.join(cwd, 'result.tar.gz')
+        if os.path.exists(tarball):
+            return JobStatus.COMPLETED
         response = requests.get('%s/job/id/%s' % (cls._host, job_id))
         response.raise_for_status()
         if "finished" not in response.text:
@@ -33,7 +45,9 @@ class JPredRunner(Runner):
         archive_url = '{0}/results/{1}/{1}.tar.gz'.format(cls._jpred4, job_id)
         arch_response = requests.get(archive_url, stream=True)
         arch_response.raise_for_status()
-        with open(os.path.join(cwd, 'result.tar.gz'), 'wb') as fp:
+        with open(tarball, 'wb') as fp:
             for chunk in arch_response.iter_content(chunk_size=4096):
                 fp.write(chunk)
+        with tarfile.open(tarball) as archive:
+            archive.extractall(cwd)
         return JobStatus.COMPLETED
