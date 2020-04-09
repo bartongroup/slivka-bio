3edebbdabf24bdc9e6a29cd30c75e4936d4eae2e
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2020-04-09 12:40:52 +0100

    Update settings path lookup in manage.py

--- manage.py
+++ manage.py
@@ -1,14 +1,15 @@
 #!/usr/bin/env python3
 
 import os
+import sys
 
+home = os.path.dirname(os.path.abspath(__file__))
+sys.path.append(home)
 SETTINGS_FILE = 'settings.yml'
 
 
 if __name__ == "__main__":
-    settings_path = os.path.join(
-        os.path.dirname(os.path.abspath(__file__)), SETTINGS_FILE
-    )
+    settings_path = os.path.join(home, SETTINGS_FILE)
     os.environ.setdefault('SLIVKA_SETTINGS', settings_path)
     try:
         import slivka.command
