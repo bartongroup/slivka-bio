e448740c7c66530da9b9846613e129f4b8595b17
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2020-04-16 17:43:43 +0100

    Switch to new slivka.cli module.

--- manage.py
+++ manage.py
@@ -4,22 +4,17 @@ import os
 import sys
 
 home = os.path.dirname(os.path.abspath(__file__))
-SETTINGS_FILE = 'settings.yaml'
-
 
 if __name__ == "__main__":
     home = os.environ.get('SLIVKA_HOME', home)
     os.environ.setdefault('SLIVKA_HOME', home)
-
-    settings_path = os.path.join(home, SETTINGS_FILE)
-    os.environ.setdefault('SLIVKA_SETTINGS', settings_path)
     sys.path.append(home)
     try:
-        import slivka.command
+        import slivka.cli
     except ImportError:
         raise ImportError(
             "Couldn't import slivka. Make sure it's installed corectly "
             "and available on you PYTHONPATH environment variable. "
             "Check if you activated virtual environment."
         )
-    slivka.command.manager()
+    slivka.cli.main()
--- wsgi.py
+++ wsgi.py
@@ -3,13 +3,8 @@ import os
 import slivka.conf.logging
 import slivka.server
 
-SETTINGS_FILE = 'settings.yml'
-
-settings_path = os.path.join(
-    os.path.dirname(os.path.abspath(__file__)),
-    SETTINGS_FILE
-)
-os.environ.setdefault('SLIVKA_SETTINGS', settings_path)
+home = os.path.dirname(os.path.abspath(__file__))
+os.environ.setdefault('SLIVKA_HOME', home)
 slivka.conf.logging.configure_logging()
 
 slivka.server.init()
