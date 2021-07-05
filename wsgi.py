3bdcfce7fa40da1cf3c47cb091f4c0414ad2bb85
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2021-07-05 17:36:07 +0100

    Remove server.init() call from wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -7,6 +7,5 @@ home = os.path.dirname(os.path.abspath(__file__))
 os.environ.setdefault('SLIVKA_HOME', home)
 slivka.conf.logging.configure_logging()
 
-slivka.server.init()
 application = app = slivka.server.create_app()
 
