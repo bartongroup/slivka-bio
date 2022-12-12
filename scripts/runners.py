a27e3ee0393308a11c8a224629bf01904dd19d2c
Mateusz Warowny <mmzwarowny@dundee.ac.uk>
2022-12-12 15:09:12 +0100

    Tidy up tafile extractall() sanitization

--- scripts/runners.py
+++ scripts/runners.py
@@ -53,25 +53,17 @@ class JPredRunner(BaseCommandRunner):
                 fp.write(chunk)
         with tarfile.open(tarball) as archive:
             def is_within_directory(directory, target):
-                
                 abs_directory = os.path.abspath(directory)
                 abs_target = os.path.abspath(target)
-            
                 prefix = os.path.commonprefix([abs_directory, abs_target])
-                
                 return prefix == abs_directory
             
-            def safe_extract(tar, path=".", members=None, *, numeric_owner=False):
+            for member in archive.getmembers():
+                member_path = os.path.join(cwd, member.name)
+                if not is_within_directory(cwd, member_path):
+                    return JobStatus.ERROR
+            archive.extractall(cwd)
             
-                for member in tar.getmembers():
-                    member_path = os.path.join(path, member.name)
-                    if not is_within_directory(path, member_path):
-                        raise Exception("Attempted Path Traversal in Tar File")
-            
-                tar.extractall(path, members, numeric_owner=numeric_owner) 
-                
-            
-            safe_extract(archive, cwd)
         return JobStatus.COMPLETED
 
     def status(self, jobs: List[Job]) -> List[JobStatus]:
